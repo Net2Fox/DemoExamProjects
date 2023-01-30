298c3a325b7b5506567a041f4d7849f7d9401aa6
Net2Fox <trakounet.fox@gmail.com>
2023-01-30 15:05:38 +0300

    Выполненное задание автосервиса

---
+++ DoeduSamGorlanov/AddEditProductWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoeduSamGorlanov
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditProductWindow.xaml
+    /// </summary>
+    public partial class AddEditProductWindow : Window
+    {
+        Product CurrentProduct;
+
+        public AddEditProductWindow(Product selectedProduct = null)
+        {
+            InitializeComponent();
+            var manufacturers = DoedySamGorlanovEntities.GetContext().Manufacturer.ToList();
+            ManufacturerComboBox.ItemsSource = manufacturers;
+            if (selectedProduct != null)
+            {
+                CurrentProduct = selectedProduct;
+                IDTextBox.Text = selectedProduct.ID.ToString();
+                ProductNameTextBox.Text = selectedProduct.Title;
+                CostTextBox.Text = selectedProduct.Cost.ToString();
+                DescriptionTextBox.Text = selectedProduct.Description;
+                MainImagePathTextBox.Text = selectedProduct.MainImagePath;
+                ManufacturerComboBox.SelectedItem = selectedProduct.Manufacturer;
+            }
+            else if(selectedProduct == null)
+            {
+                IDTextBlock.Visibility = Visibility.Hidden;
+                IDTextBox.Visibility = Visibility.Hidden;
+                SaveButton.Content = "Добавить";
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(CurrentProduct == null)
+            {
+                CurrentProduct = new Product();
+                CurrentProduct.Title = ProductNameTextBox.Text;
+                try
+                {
+                    CurrentProduct.Cost = Convert.ToDecimal(CostTextBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Укажите корректное значение стоимости!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                CurrentProduct.Description = DescriptionTextBox.Text;
+                CurrentProduct.MainImagePath = MainImagePathTextBox.Text;
+                if (ManufacturerComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите производителя!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                CurrentProduct.Manufacturer = ManufacturerComboBox.SelectedItem as Manufacturer;
+                CurrentProduct.IsActive = true;
+                try
+                {
+                    DoedySamGorlanovEntities.GetContext().Product.Add(CurrentProduct);
+                    DoedySamGorlanovEntities.GetContext().SaveChanges();
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла непредвиденная ошибка! Обратитесь к системному администратору!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                MessageBox.Show("Товар был успешно добавлен!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                CurrentProduct.Title = ProductNameTextBox.Text;
+                try
+                {
+                    CurrentProduct.Cost = Convert.ToDecimal(CostTextBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Укажите корректное значение стоимости!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                CurrentProduct.Description = DescriptionTextBox.Text;
+                CurrentProduct.MainImagePath = MainImagePathTextBox.Text;
+                if(ManufacturerComboBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите производителя!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                CurrentProduct.Manufacturer = ManufacturerComboBox.SelectedItem as Manufacturer;
+                try
+                {
+                    DoedySamGorlanovEntities.GetContext().SaveChanges();
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла непредвиденная ошибка! Обратитесь к системному администратору!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                MessageBox.Show("Товар был успешно изменён!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ DoeduSamGorlanov/Pages/ProductListPage.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoeduSamGorlanov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        int ViewedCountItems = 0;
+        int AllCountItems = 0;
+
+        public ProductListPage()
+        {
+            InitializeComponent();
+            Update();
+            ViewCountTextBlock.Text = $"{ViewedCountItems} из {AllCountItems}";
+            var manufacturers = DoedySamGorlanovEntities.GetContext().Manufacturer.ToList();
+            Manufacturer mnAll = new Manufacturer();
+            mnAll.Name = "Все производители";
+            manufacturers.Insert(0, mnAll);
+            ManufacturerComboBox.ItemsSource = manufacturers;
+        }
+
+        internal void Update()
+        {
+            var products = DoedySamGorlanovEntities.GetContext().Product.ToList();
+            ProductsListView.ItemsSource = products;
+            ViewedCountItems = products.Count();
+            AllCountItems = products.Count();
+        }
+
+        private void Sort()
+        {
+            var products = DoedySamGorlanovEntities.GetContext().Product.ToList();
+            if (!String.IsNullOrEmpty(TitleTextBox.Text))
+            {
+                products = products.Where(p => p.Title.ToLower().Contains(TitleTextBox.Text.ToLower())).ToList();
+            }
+            if (ManufacturerComboBox.SelectedIndex != 0)
+            {
+                products = products.Where(p => p.Manufacturer == ManufacturerComboBox.SelectedItem).ToList();
+            }
+            if (CostAscRadioButton.IsChecked == true)
+            {
+                products = products.OrderBy(p => p.Cost).ToList();
+            }
+            if (CostDescRadioButton.IsChecked == true)
+            {
+                products = products.OrderByDescending(p => p.Cost).ToList();
+            }
+            ViewedCountItems = products.Count();
+            ViewCountTextBlock.Text = $"{ViewedCountItems} из {AllCountItems}";
+            ProductsListView.ItemsSource = products;
+        }
+
+        private void TitleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void ManufacturerComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void CostAscRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void CostDescRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ProductsListView.SelectedItem != null)
+                {
+                    var selectedProduct = ProductsListView.SelectedItem as Product;
+                    if (selectedProduct.ProductSale.FirstOrDefault() == null)
+                    {
+                        if (selectedProduct.Product1 != null)
+                        {
+                            foreach (Product attachedProduct in selectedProduct.Product1)
+                            {
+                                if (attachedProduct.ProductSale.FirstOrDefault() != null)
+                                {
+                                    MessageBox.Show("Данный товар нельзя удалить!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                                    return;
+                                }
+                            }
+                            DoedySamGorlanovEntities.GetContext().Product.RemoveRange(selectedProduct.Product1);
+                        }
+                        DoedySamGorlanovEntities.GetContext().Product.Remove(selectedProduct);
+                        DoedySamGorlanovEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Товар был успешно удалён!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Update();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Данный товар нельзя удалить!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Произошла непредвиденная ошибка! Обратитесь к системному администратору!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditProductWindow addEditProduct = new AddEditProductWindow();
+            addEditProduct.Show();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductsListView.SelectedItem != null)
+            {
+                AddEditProductWindow addEditProduct = new AddEditProductWindow(ProductsListView.SelectedItem as Product);
+                addEditProduct.Show();
+
+            }
+        }
+    }
+}
