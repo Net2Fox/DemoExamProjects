01ed37693b5fab4fdb8ec55e2f2614cc54c99408
Net2Fox <trakounet.fox@gmail.com>
2023-02-06 23:24:16 +0300

    Добавлен скрипт

---
+++ GIBDD_Gorlanov_619/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIBDD_Gorlanov_619
+{
+    internal class Core
+    {
+    }
+}
