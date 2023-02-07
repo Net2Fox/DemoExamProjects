de77f6b07bb1a3dca3f351d2977a601f81e0fbcf
Владимир Горланов <197703@edu.fa.ru>
2023-02-07 14:21:39 +0300

    Добавлены картинки

--- GIBDD_Gorlanov_619/Core.cs
+++ GIBDD_Gorlanov_619/Core.cs
@@ -8,5 +8,15 @@ namespace GIBDD_Gorlanov_619
 {
     internal class Core
     {
+        private static GIBDDEntities _context;
+
+        public static GIBDDEntities GetContext()
+        {
+            if(_context == null)
+            {
+                _context = new GIBDDEntities();
+            }
+            return _context;
+        }
     }
 }
