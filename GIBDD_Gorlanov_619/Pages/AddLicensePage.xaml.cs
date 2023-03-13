364b2d2cfc83a58adaafe739c64c0a08fab2011a
Net2Fox <trakounet.fox@gmail.com>
2023-03-13 18:41:51 +0300

    Мелкие правки

--- GIBDD_Gorlanov_619/Pages/AddLicensePage.xaml.cs
+++ GIBDD_Gorlanov_619/Pages/AddLicensePage.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using GIBDD_Gorlanov_619.Windows;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GIBDD_Gorlanov_619.Pages
 {
