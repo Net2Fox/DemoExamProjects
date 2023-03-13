3cd2ba08692ad13690ac06d0e695b6663144e4f0
Net2Fox <trakounet.fox@gmail.com>
2023-03-13 18:11:15 +0300

    Мелкие правки

--- 619_Gorlanov_KingIT/Classes/Core.cs
+++ 619_Gorlanov_KingIT/Classes/Core.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace _619_Gorlanov_KingIT.Classes
+﻿namespace _619_Gorlanov_KingIT.Classes
 {
     internal class Core
     {
